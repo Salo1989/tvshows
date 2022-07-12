76d7ced18fc72c4dabcd184d4c414e32732a913e
Salomon Elguera <106698702+Salo1989@users.noreply.github.com>
2022-07-12 21:07:39 +0300

    Add files via upload

---
+++ TV shows.py
@@ -0,0 +1,16 @@
+print(" Welcome to TV land")
+print(" We determine who you are based on your TV preference")
+user_input=input(" Enter the TV show you most like Sopranos, Breaking Bad, or The Wire:")
+ans_one="Sopranos"
+ans_two="Breaking Bad"
+ans_three="The Wire"
+if user_input==ans_one:
+    print(" The Sopranos is a great television show")
+    print(" You should consider listening to Frank Collota on YouTube")
+elif user_input==ans_two:
+    print(" There are many shows on Nat Geo about the war on drugs")
+    print(" I recomend watching Drug Pin on Nat Geo")
+    print(" Great show and if you like Breaking Bad you will love this show")
+else:
+    print(" The Wire is ok but you should consider watching other shows like")
+    print(" The other two shows mentioned previously")
